a76376133241b0a7dd250a68103a99c443936b3b
Faris Ghilmany <fghilmany62@gmail.com>
2021-10-15 17:20:01 +0700

    Initial commit

---
+++ app/src/main/java/com/fghilmany/dietmealapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fghilmany.dietmealapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fghilmany.dietmealapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
