e5edf77c71972befefd2cc6d2160968cd02f40fe
Faris Ghilmany <fghilmany62@gmail.com>
2021-10-25 08:26:59 +0700

    Update 24/10/21

--- app/src/main/java/com/fghilmany/dietmealapp/ui/daily_activity/DailyActivityFragment.kt
+++ app/src/main/java/com/fghilmany/dietmealapp/ui/daily_activity/DailyActivityFragment.kt
@@ -5,16 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.fghilmany.dietmealapp.R
+import com.fghilmany.dietmealapp.databinding.FragmentDailyActivityBinding
 
 class DailyActivityFragment : Fragment() {
+
+    private var _binding: FragmentDailyActivityBinding? = null
+    private val binding get() = _binding!!
+
+    private var identityPosition: Int? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_activity, container, false)
+        _binding = FragmentDailyActivityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding){
+
+            rgActivity.setOnCheckedChangeListener { _, position ->
+                identityPosition = position
+            }
+
+            btFinish.setOnClickListener {
+
+                if (identityPosition == null){
+                    Toast.makeText(requireContext(), "Kamu belum memilih", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+
+                val toIdentity = DailyActivityFragmentDirections.actionDailyActivityFragmentToHomeActivity()
+                view.findNavController().navigate(R.id.action_dailyActivityFragment_to_homeActivity)
+            }
+        }
     }
 
 
--- app/src/main/java/com/fghilmany/dietmealapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/fghilmany/dietmealapp/ui/main/MainActivity.kt
@@ -3,10 +3,17 @@ package com.fghilmany.dietmealapp.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fghilmany.dietmealapp.R
+import com.fghilmany.dietmealapp.databinding.ActivityHomeBinding
+import com.fghilmany.dietmealapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
